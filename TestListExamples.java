ceab646c2d4f5b8ecbea847b69ecd739ff50ab99
Daniel Rousseau <danielrousseau@Daniels-MacBook-Pro-2.local>
2022-11-09 15:47:40 -0800

    xd

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,59 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+
+class longerThanFive implements StringChecker {
+	public boolean checkString(String s){
+		if(s.length() > 5){return true;}
+		return false;
+	}
+}
 
 public class TestListExamples {
-  // Write your grading tests here!
+    @Test
+	public void testFilter() {
+		List<String> input1 = new ArrayList<>();
+		input1.add("a");
+		input1.add("longone");
+		input1.add("four");
+		input1.add("verlongstring");
+	    longerThanFive sc = new longerThanFive();
+	    input1 = ListExamples.filter(input1, sc);
+		
+		List<String> input2 = new ArrayList<>();
+		input2.add("longone");
+		input2.add("verlongstring");
+		assertEquals(input2, input1);		
+	}
+
+    @Test
+	public void testMerge() {
+		List<String> input1 = new ArrayList<>();
+		List<String> input2 = new ArrayList<>();
+		List<String> input3 = new ArrayList<>();
+        List<String> input4 = new ArrayList<>();
+
+		input1.add("a");
+		input1.add("b");
+		input1.add("c");
+		input1.add("d");
+    
+		input2.add("a");
+		input2.add("a");
+		input2.add("a");
+		input2.add("e");
+
+        input3.add("a");
+		input3.add("a");
+		input3.add("a");
+		input3.add("a");
+		input3.add("b");
+		input3.add("c");
+		input3.add("d");
+		input3.add("e");
+
+	    input4 = ListExamples.merge(input1,input2);
+	
+		assertEquals(input3, input4);		
+	}
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,29 @@
-# Create your grading script here
-
-set -e
-
+# Clear current student dir and add files from github
 rm -rf student-submission
+mkdir -p /Users/danielrousseau/Documents/CSE15L/list-examples-grader/student-submission
 git clone $1 student-submission
+
+# Copy tests into student folder
+cp TestListExamples.java student-submission
+
+# Check if student submission compiiles
+javac student-submission/ListExamples.java 
+if [[ $? -ne 0 ]]
+then 
+    echo "##########################################" 
+    echo ""
+    echo "Compilation Error, fix your mistake."
+    echo "Your grade is 0/100"
+    exit 1
+fi
+
+
+# Compile and run tests
+cd student-submission
+javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+java -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > ../output.txt
+cd ../
+
+grep -E "Failures|OK" output.txt
+
 
